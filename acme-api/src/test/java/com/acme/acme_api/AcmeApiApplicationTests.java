8315848e964a33f1d60dc15a0023b0b1d34dcc41
acheird <acheird@gmail.com>
2026-09-15 14:25:50 +0300

    feat: add Spring Boot skeleton for acme-api (unbuilt endpoints yet)

---
+++ acme-api/src/test/java/com/acme/acme_api/AcmeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.acme_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
